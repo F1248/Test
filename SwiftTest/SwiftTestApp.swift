509cf47f5d301d08856912ecacf57f73aae91e87
F1248 <f1248@mailbox.org>
2024-12-12 19:02:13 +0100

    Test

--- SwiftTest/SwiftTestApp.swift
+++ SwiftTest/SwiftTestApp.swift
@@ -4,13 +4,7 @@ import SwiftUI
 struct SwiftTestApp: App {
     var body: some Scene {
         WindowGroup {
-			VStack {
-				Image(systemName: "globe")
-					.imageScale(.large)
-					.foregroundStyle(.tint)
-				Text("Hello, world!")
-			}
-			.padding()
+			Text("Hello, world!")
         }
     }
 }
